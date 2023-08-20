7a1ee2062889bac7c9539f9b85b19e7a58687e1d
Bethany Tormino <torminobethany@gmail.com>
2023-08-20 13:16:14 -0500

    Worked on Bonus missions, but need to walk away from this to be able to figure out what exactly I need to do.

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -2,15 +2,29 @@ package org.launchcode.java.studios.areaofacircle;
 
 import java.util.Scanner;
 public class Area {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        double radius;
+
+        double radius = 0;
         double area;
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a radius: ");
-        radius = input.nextDouble();
-        input.close();
 
+        System.out.println("Enter a radius: ");
+        while (true) {
+            if (!input.hasNextDouble()) {
+                System.out.println("Sorry, your radius can't be a letter, negative number, or symbol.");
+                break;
+            }
+            System.out.println("Enter a radius: ");
+            //else if (input > 0) {
+            //  System.out.println("Sorry, your radius can't be a letter, negative number, or symbol.");
+            //  input.next();
+            radius = input.nextDouble();
+        }
+        //radius = input.nextDouble();
+        System.out.println("Thank you! Your radius is " + radius + " .");
+        input.close();
+        //}
         area = Circle.getArea(radius);
 
         System.out.println("The area of a circle of a radius of " + radius + " is " + area + " .");
